e0d5b981890f37e7287ea5fa001c51ea0a1d37d5
Hokuto Hoshi <hok.kanny@gmail.com>
2015-12-24 11:20:14 +0900

    Initial commit: git-secrets formula

---
+++ git-secrets.rb
@@ -0,0 +1,11 @@
+class GitSecrets < Formula
+  desc "Prevents you from committing passwords and other sensitive information to a git repository."
+  homepage "https://github.com/awslabs/git-secrets"
+  url "https://github.com/awslabs/git-secrets/archive/1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "3fac12be9e1cd0f67daf801db17ebd9b1b302c2cc22e3b9c52aca7e55fdcd84f"
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+end
