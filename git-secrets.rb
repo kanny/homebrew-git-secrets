a2dde6180143dd6f4a6fe900eab329048b6d52d4
Hokuto Hoshi <hok.kanny@gmail.com>
2015-12-24 14:31:06 +0900

    Fix by brew-audit

--- git-secrets.rb
+++ git-secrets.rb
@@ -1,8 +1,7 @@
 class GitSecrets < Formula
-  desc "Prevents you from committing passwords and other sensitive information to a git repository."
+  desc "Prevents you from committing sensitive information to a git repo"
   homepage "https://github.com/awslabs/git-secrets"
   url "https://github.com/awslabs/git-secrets/archive/1.0.0.tar.gz"
-  version "1.0.0"
   sha256 "3fac12be9e1cd0f67daf801db17ebd9b1b302c2cc22e3b9c52aca7e55fdcd84f"
 
   head "https://github.com/awslabs/git-secrets"
@@ -15,5 +14,4 @@ class GitSecrets < Formula
     system "git", "init"
     system "git", "secrets", "--install"
   end
-
 end
